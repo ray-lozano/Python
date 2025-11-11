80f85ed20072aa376c74a44834366926ab7cffa7
Raymond Lozano <ray-lozano@live.com>
2025-11-10 19:30:52 -0800

    initializing

---
+++ 100_days_of_code/day_1_project_band_name_generator.py
@@ -0,0 +1,11 @@
+## Create a greeting for the program
+print("Welcome to the band name generator!")
+
+## Ask the user for the city that they grew up in and store it in a variable
+city = input("Enter the name of the city that you grew up in: ")
+
+## Ask the user for the name of a pet and store it in a variable
+pet_name = input("Enter the name of a pet: ")
+
+## Combine the name of their city and pet and show them their band name.
+print(f"Your band name could be {city} {pet_name}")
